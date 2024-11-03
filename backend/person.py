b017c00cbec6ed3927d9b841d04ce90e032591c5
unknown <grays.rf@gmail.com>
2024-11-03 10:49:47 +0300

    add class Person

---
+++ backend/person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, fname='', sname='', lname='', age=0, sum=0):
+        self.f_name_ = fname
+        self.s_name_ = sname
+        self.l_name_ = lname
+        self.age_ = age
+        self.sum_ = sum
