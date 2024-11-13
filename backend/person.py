bbc513d09b5c76673198d43d27453a8fb2754a1d
unknown <grays.rf@gmail.com>
2024-11-13 20:51:07 +0300

    add function check parameters

--- backend/person.py
+++ backend/person.py
@@ -42,41 +42,61 @@ class Person:
             fields = ', '.join(attributes.keys())
             placeholders = ', '.join(['?'] * len(attributes))
             values = tuple(attributes.values())
-            # existing_record = []
+
             cursor = self._conn.cursor()
+            cursor.execute(f"PRAGMA table_info(Users)")
+            column_names = [row[1] for row in cursor.fetchall()]
             cursor.execute(f"SELECT * FROM Users WHERE ({fields}) = ({placeholders})",
                             (values))
             existing_record = cursor.fetchall()
-            print(len(existing_record))
-            # if cursor.fetchall()[0] in existing_record[:, 0]:
-            #     print('test')
-            # existing_record.append(cursor.fetchall())
-
-            # all_empty = all(not inner for inner in existing_record)
             
-            # if all_empty:
-            #     print('пусто')
-            #     self.add_in_db()
-            # else:
-            #     print('не пусто')
-            #     self.check_changes()
+            self._db_data = {column_names[i]:existing_record[i] for i in range(len(existing_record))}
 
+            if len(self._db_data) == 0:
+                print('Try to change find-param or add user')
+                # self.add_in_db()
+            elif len(self._db_data) == 1:
+                print('Need to check parameters')
+                self.check_parameters()
+            elif len(self._db_data) > 1:
+                print('Check duplicate')
 
-    # def add_in_db(self):
+    def add_in_db(self):
     
-    #     attributes = {k: v for k, v in self.__dict__.items() if not k.startswith('_')}
+        attributes = {k: v for k, v in self.__dict__.items() if not k.startswith('_')}
 
-    #     fields = ', '.join(attributes.keys())
-    #     placeholders = ', '.join(['?'] * len(attributes))
-    #     values = tuple(attributes.values())
+        fields = ', '.join(attributes.keys())
+        placeholders = ', '.join(['?'] * len(attributes))
+        values = tuple(attributes.values())
 
-    #     cursor = self._conn.cursor()
-    #     insert_query = f"INSERT INTO Users ({fields}) VALUES ({placeholders})"
-    #     cursor.execute(insert_query, values)
+        cursor = self._conn.cursor()
+        insert_query = f"INSERT INTO Users ({fields}) VALUES ({placeholders})"
+        cursor.execute(insert_query, values)
         
-    #     self._conn.commit()
+        self._conn.commit()
 
-    # def check_changes():
+    def check_parameters(self):
+        attributes = {k: v for k, v in self.__dict__.items() if not k.startswith('_')}
+        common_keys = set(attributes.keys()) & set(self._db_data.keys())
+
+        different_key = set(self._db_data.keys()) - set(attributes.keys())
+
+        different_values = {key: (attributes[key], self._db_data[key]) for key in common_keys if attributes[key] != self._db_data[key]}
+        if not (different_values and different_key):
+            print('Nothing to change')
+        elif different_values:
+            print(different_values)
+        elif different_key:
+            print(different_key)
+
+
+            
+        else:
+            print('Nothing to change')
+        # for atr in attributes:
+        #     if 
+            # print(attributes)
+
 
     # def save_to_db(self, conn):
     #     print(self.__dict__.items())
